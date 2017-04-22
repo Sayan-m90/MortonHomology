f37fafecf8d88a334f6626bad3f33f172091750e
Sayan Mandal <sayan.mndl90@gmail.com>
2017-04-22 11:01:33 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-LIBS="-lann -lGLEW -lglfw3 -lGL -lglut -lboost_system -lX11 -lXxf86vm -lpthread -ldl -lXrandr -lXinerama -lXcursor -lboost_filesystem -lboost_program_options"
+LIBS="-lann -lboost_system -lboost_filesystem -lboost_program_options -I/usr/local/include/opencv -I/usr/local/include -L/usr/local/lib -lopencv_ml -lopencv_objdetect -lopencv_shape -lopencv_stitching -lopencv_superres -lopencv_videostab -lopencv_calib3d -lopencv_features2d -lopencv_highgui -lopencv_videoio -lopencv_imgcodecs -lopencv_video -lopencv_photo -lopencv_imgproc -lopencv_flann -lopencv_core"
 
-g++ -o morton --std=c++11 *.cpp Wrappers/*.cpp SimPers/*.cpp Graphs/*.cpp GIComplex/*.cpp $LIBS 
+g++ -g  -o pers --std=c++11  *.cpp Wrappers/*.cpp SimPers/*.cpp Graphs/*.cpp GIComplex/*.cpp  $LIBS 
